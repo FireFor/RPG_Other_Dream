9ae5874be4b38858dbcb3ac4d219a0462c6afcbe
Nicolas Tritz <tritz.nicolas@gmail.com>
2012-10-18 20:20:00 +0200

    Fichier exemple "module" gamestate

---
+++ tests/_exemple.lua
@@ -0,0 +1,66 @@
+--[[
+exemple = gamestate.new()
+
+--état du jeu
+function exemple:init()
+	print('<exemple:init()>')
+	
+	print('</exemple:init()>')
+end
+
+function exemple:enter()
+	print('<exemple:enter()>')
+	
+	print('</exemple:enter()>')
+end
+
+function exemple:leave()
+	print('<exemple:leave()>')
+	
+	print('</exemple:leave()>')
+end
+
+--évènements
+function exemple:focus(focus)
+	
+end
+
+function exemple:keypressed(key, unicode)
+	
+end
+
+function exemple:keyreleased(key)
+	
+end
+
+function exemple:mousepressed(x, y, button)
+	
+end
+
+function exemple:mousereleased(x, y, button)
+	
+end
+
+function exemple:joystickpressed(joystick, button)
+	
+end
+
+function exemple:joystickreleased(joystick, button)
+	
+end
+
+--calculs
+function exemple:update(dt)
+	
+end
+
+--dessins
+function exemple:draw()
+	
+end
+
+--adieu
+function exemple:quit()
+	
+end
+]]--
